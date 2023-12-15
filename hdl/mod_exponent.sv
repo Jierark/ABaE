e797930e8bda346f99aa5d999f0c52354161b975
Joseph Kim <dudgns0625@gmail.com>
2023-12-14 19:14:48 -0500

    Adjustments to modular exponent

--- hdl/mod_exponent.sv
+++ hdl/mod_exponent.sv
@@ -18,23 +18,53 @@ module mod_exponent #(parameter WIDTH = 512) (
     output logic valid_out,
     output logic busy_out
     );
-    typedef enum {IDLE=0,COMPUTING=1,DONE=2,UPDATE_PRODUCT=3,REDUCE=5,UPDATE_BASE=4,NEXT=6,LAST_REDUCTION=7,WAITING=8} states;
+    typedef enum {IDLE=0,COMPUTING=1,DONE=2,UPDATE_PRODUCT=3,REDUCE=5,UPDATE_BASE=4,NEXT=6,LAST_REDUCTION=7,WAITING=8,MULTIPLIER_START=9,MULTIPLIER_DONE=10} states;
     states state;
     logic [WIDTH-1:0] running_product, current_base;
     logic [$clog2(WIDTH)-1:0] current_index;
     logic [WIDTH*2:0] reduce_input1, reduce_input2;
     logic [WIDTH-1:0] reduce_output1, reduce_output2;
     logic finished, finished2; // used to keep track of the reductions finishing
+    // multipliers
+        logic mult_valid_in, mult_valid_out, mult_busy;
+    logic [WIDTH-1:0] mult_a, mult_b;
+    logic [WIDTH*2-1:0] mult_out;
+
+    multiplier #(.WIDTH(WIDTH))
+     multiplier(
+        .clk_in(clk_in),
+        .rst_in(rst_in),
+        .a_in(mult_a),
+        .b_in(mult_b),
+        .valid_in(mult_valid_in),
+        .c_out(mult_out),
+        .valid_out(mult_valid_out),
+        .busy_out(mult_busy)
+     );
+    logic [WIDTH-1:0] mark_a, mark_b;
+    logic [WIDTH*2-1:0] mark_out;
+    logic markiplier_valid_in, markiplier_valid_out, markiplier_busy;
+    multiplier #(.WIDTH(WIDTH))
+     markiplier(.clk_in(clk_in),
+                .rst_in(rst_in),
+                .a_in(mark_a),
+                .b_in(mark_b),
+                .valid_in(markiplier_valid_in),
+                .c_out(mark_out),
+                .valid_out(markiplier_valid_out),
+                .busy_out(markiplier_busy)
+                );
+
     // Used to keep track of the current base
     logic reduction_valid_in, reduction_valid_out, reduction_busy;
     mont_reduction #(.WIDTH(WIDTH))
      reduction(.clk_in(clk_in),
                 .rst_in(rst_in),
-                .x_mont(reduce_input1),
+                .x_mont(mult_out),
                 .N(modulo),
                 .R(R),
                 .N_prime(inv_modulo),
-                .valid_in(reduction_valid_in),
+                .valid_in(mult_valid_out),
                 .x_out(reduce_output1),
                 .valid_out(reduction_valid_out),
                 .busy_out(reduction_busy)
@@ -44,11 +74,11 @@ module mod_exponent #(parameter WIDTH = 512) (
     mont_reduction #(.WIDTH(WIDTH))
      reduction2(.clk_in(clk_in),
                 .rst_in(rst_in),
-                .x_mont(reduce_input2),
+                .x_mont(mark_out),
                 .N(modulo),
                 .R(R),
                 .N_prime(inv_modulo),
-                .valid_in(reduction2_valid_in),
+                .valid_in(markiplier_valid_out),
                 .x_out(reduce_output2),
                 .valid_out(reduction2_valid_out),
                 .busy_out(reduction2_busy)
@@ -68,8 +98,6 @@ module mod_exponent #(parameter WIDTH = 512) (
             state <= IDLE;
             busy_out <= 0;
             finished <= 0;
-            reduce_input1 <= 0;
-            reduce_input2 <= 0;
         end else begin
             case (state)
                 IDLE: begin
@@ -94,15 +122,18 @@ module mod_exponent #(parameter WIDTH = 512) (
                     end
                 end
                 UPDATE_BASE: begin
-                    reduce_input1 <= current_base * current_base;
-                    reduction_valid_in <= 1'b1;
+                    mult_a <= current_base;
+                    mult_b <= current_base;
+                    mult_valid_in <= 1'b1;
                     state <= REDUCE;
                 end
                 UPDATE_PRODUCT: begin
-                    reduce_input1 <= current_base * current_base;
-                    reduction_valid_in <= 1'b1;
-                    reduce_input2 <= running_product * current_base;
-                    reduction2_valid_in <= 1'b1;
+                    mult_a <= current_base;
+                    mult_b <= current_base;
+                    mult_valid_in <= 1'b1;
+                    mark_a <= current_base;
+                    mark_b <= running_product;
+                    markiplier_valid_in <= 1'b1;
                     state <= REDUCE;
                 end
                 REDUCE: begin
@@ -118,8 +149,8 @@ module mod_exponent #(parameter WIDTH = 512) (
                         if (finished == 1'b1 && finished2 == 1'b1) begin
                             state <= NEXT;
                         end
-                        reduction_valid_in <= 0;
-                        reduction2_valid_in <= 0;
+                        mult_valid_in <= 0;
+                        markiplier_valid_in <= 0;
                     end else begin
                         if (reduction_valid_out) begin
                             finished <= 1'b1;
@@ -128,7 +159,7 @@ module mod_exponent #(parameter WIDTH = 512) (
                         if (finished) begin
                             state <= NEXT;
                         end
-                        reduction_valid_in <= 0;
+                        mult_valid_in <= 0;
                     end
                 end
                 NEXT: begin
@@ -138,8 +169,9 @@ module mod_exponent #(parameter WIDTH = 512) (
                     finished <= 0;
                 end
                 LAST_REDUCTION: begin
-                    reduce_input2 <= running_product;
-                    reduction2_valid_in <= 1'b1;
+                    mark_a <= running_product; // terribly slow but oops
+                    mark_b <= 1'b1;
+                    markiplier_valid_in <= 1'b1;
                     state <= WAITING;
                 end
                 WAITING: begin
